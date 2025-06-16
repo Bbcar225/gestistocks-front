7fa469c7a23c108e7b9339ec113b78a9b38e95e2
Bbcar225 <boubacarly93@gmail.com>
2025-06-16 11:22:47 +0000

    Update ButtonDownloadInvoiceSale.tsx

--- src/components/atoms/ButtonDownloadInvoiceSale.tsx
+++ src/components/atoms/ButtonDownloadInvoiceSale.tsx
@@ -1,29 +1,32 @@
 import {FaFileInvoice} from "react-icons/fa";
-import {Button} from "antd";
+import {Button, Tooltip} from "antd";
 import {useSaleDownloadInvoice} from "../../hooks/Api/tenant/SaleHookAPI.ts";
 
-export default function ButtonDownloadInvoiceSale({id, text = 'Reçu'}: { id: number, text?: string|null }) {
+export default function ButtonDownloadInvoiceSale({id, text = 'Reçu'}: { id: number, text?: string | null }) {
 	const reqSaleDownloadInvoice = useSaleDownloadInvoice()
 	
-	return <Button
-		type='primary'
-		icon={<FaFileInvoice/>}
-		loading={reqSaleDownloadInvoice.isLoading}
-		onClick={() => {
-			reqSaleDownloadInvoice.mutate({id}, {
-				onSuccess: (res) => {
-					try {
-						const file = new Blob([res], {type: 'application/pdf'});
-						const fileURL = URL.createObjectURL(file);
-						
-						window.open(fileURL, '_blank');
-					} catch (error) {
-						console.error(error)
+	return <Tooltip title={!text ? 'Reçu' : null}>
+		<Button
+			variant='filled'
+			color='primary'
+			icon={<FaFileInvoice/>}
+			loading={reqSaleDownloadInvoice.isLoading}
+			onClick={() => {
+				reqSaleDownloadInvoice.mutate({id}, {
+					onSuccess: (res) => {
+						try {
+							const file = new Blob([res], {type: 'application/pdf'});
+							const fileURL = URL.createObjectURL(file);
+							
+							window.open(fileURL, '_blank');
+						} catch (error) {
+							console.error(error)
+						}
 					}
-				}
-			})
-		}}
-	>
-		{text && text}
-	</Button>
+				})
+			}}
+		>
+			{text && text}
+		</Button>
+	</Tooltip>
 }
